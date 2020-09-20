fa902a5ec6b1bcb06f6944ddbac9a592b603f6d9
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-09-20 14:30:34 -0700

    wtf

---
+++ src/app/components/modal/modal.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { ModalComponent } from './modal.component'
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NzModalModule
+  ]
+})
+export class ModalModule { }
