9ecb583e85765680915249a3d5ba7c36ca290489
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-09-24 01:53:35 -0700

    lacerated hand

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -2,8 +2,17 @@ import {NgModule} from '@angular/core';
 import {APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
+import { environment } from '../environments/environment'
 
-const uri = 'http://localhost:4000'; // <-- add the URL of the GraphQL server here
+let backendURL
+
+if(environment.production) {
+  backendURL = "https://thawing-depths-55365.herokuapp.com/"
+} else {
+  backendURL = "http://localhost:4000"
+}
+
+const uri = backendURL; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
