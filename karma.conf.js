3332b4846dbd7d315246b4555f91b2899a9a59e6
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-09-25 00:29:22 -0700

    added a package

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome', 'Chrome_without_security'],
+    customLauncher: {
+      Chrome_without_security: {
+        base: 'Chrome',
+        flags: ['--disable-web-security', '--disable-site-isolation-trials']
+      }
+    }
     singleRun: false,
     restartOnFileChange: true
   });
